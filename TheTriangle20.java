ffa7974b7e6b354cfbf625b604e1596902c88fe8
PratamaAngga <anggashillnoir@gmail.com>
2024-11-01 14:15:22 +0700

    lab 3 after question

--- TheTriangle20.java
+++ TheTriangle20.java
@@ -2,15 +2,14 @@ import java.util.Scanner;
 public class TheTriangle20 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int i = 0, numInput;
+        int i, numInput;
         String s = "";
         System.out.print("Input some number: ");
         numInput = input.nextInt();
 
-        while (i < numInput) {
+        for (i=0; i < numInput; i++) {
             s += "* ";
             System.out.println(s);
-            i++;
         }
     }
 }
