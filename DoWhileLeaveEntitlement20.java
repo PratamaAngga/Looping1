8dc62789d2b1cfd004d1c9fec14595ca1e2063b2
PratamaAngga <anggashillnoir@gmail.com>
2024-11-01 15:13:43 +0700

    lab 4 question 3

---
+++ DoWhileLeaveEntitlement20.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class DoWhileLeaveEntitlement20 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int leaveEntitlement = 0, numLeave;
+        String confirmation;
+        System.out.print("Input the number of leave entitlement: ");
+        leaveEntitlement = input.nextInt();
+        input.nextLine();
+        
+        do {
+            System.out.print("Do you want to take a leave (y/n)? ");
+            confirmation = input.nextLine();
+            
+            if (confirmation.equalsIgnoreCase("y")) {
+                System.out.print("How many day(s)? ");
+                numLeave = input.nextInt();
+                input.nextLine();
+
+                if (numLeave <= leaveEntitlement) {
+                    leaveEntitlement -= numLeave;
+                    System.out.println("Remaining leave entitlement: " + leaveEntitlement);
+                } else {
+                    System.out.println("You don't have enough leave entitlement! Please enter a valid number!");
+                    continue;
+                }
+            }
+        } while (leaveEntitlement > 0);
+
+        if (leaveEntitlement == 0) {
+            System.out.println("You have used up all your leave entitlement.");
+        }
+    }
+}
